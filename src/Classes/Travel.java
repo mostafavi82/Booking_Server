a9588aec303966e83e95158944c9bbd862aeac0b
s.h.m <s.h.mostafavi82@gmail.com>
2023-06-28 18:44:42 +0330

    update

--- src/Classes/Travel.java
+++ src/Classes/Travel.java
@@ -5,7 +5,7 @@ public class Travel {
     String origin;
     String destination;
     int remainingPassengers;
-    int soldOut;
+    int capacity;
     String departureTime;
     String arrivalTime;
     String travelTime;
@@ -14,12 +14,12 @@ public class Travel {
     int id;
 
 
-    Travel(String companyName,String origin,String destination,int remainingPassengers ,int soldOut ,String departureTime,String arrivalTime,String travelTime, int cost , String travelClass,int id){
+    Travel(String companyName,String origin,String destination,int remainingPassengers ,int capacity ,String departureTime,String arrivalTime,String travelTime, int cost , String travelClass,int id){
         this.companyName = companyName;
         this.origin = origin;
         this.destination = destination;
         this.remainingPassengers = remainingPassengers;
-        this.soldOut = soldOut;
+        this.capacity = capacity;
         this.departureTime = departureTime;
         this.arrivalTime = arrivalTime;
         this.travelTime = travelTime;
